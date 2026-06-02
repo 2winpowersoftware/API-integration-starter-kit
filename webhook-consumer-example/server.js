e35750f973f65376e6be1c20ffde2f527cbd0a5b
Andrew Price <165882516+2winpowersoftware@users.noreply.github.com>
2026-06-02 08:44:27 +0300

    Create server.js

---
+++ webhook-consumer-example/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/webhook", (req, res) => {
+  console.log(req.body);
+  res.sendStatus(200);
+});
+
+app.listen(3000);
